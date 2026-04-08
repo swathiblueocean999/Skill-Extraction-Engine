e17ee478c5f84e85848e9a8637eb8e047b4e3efe
Swathi tp <swathisparkly2266@gmail.com>
2026-04-08 19:49:56 +0530

    Add files via upload

---
+++ weights.py
@@ -0,0 +1,14 @@
+WEIGHTS = {
+    "default": {
+        "skill": 0.4,
+        "experience": 0.3,
+        "education": 0.1,
+        
+    },
+    "fresher": {
+        "skill": 0.5,
+        "experience": 0.1,
+        "education": 0.2,
+        
+    }
+}
