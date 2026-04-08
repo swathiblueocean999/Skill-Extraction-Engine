b1695992bc22b9a4e471301ed447d3242c36620d
Swathi tp <swathisparkly2266@gmail.com>
2026-04-08 19:49:19 +0530

    Add files via upload

---
+++ scorer.py
@@ -0,0 +1,30 @@
+
+
+def calculate_skill_score(resume_skills, jd_skills):
+    if not jd_skills:
+        return 0
+    matched = set(resume_skills).intersection(set(jd_skills))
+    return len(matched) / len(jd_skills)
+
+
+def calculate_experience_score(resume_exp, jd_exp):
+    if jd_exp == 0:
+        return 0
+    return min(resume_exp / jd_exp, 1)
+
+
+def calculate_education_score(resume_edu, jd_edu):
+    if resume_edu == jd_edu:
+        return 1
+    return 0.5
+
+
+
+def calculate_final_score(scores, weights):
+    final = (
+        scores["skill"] * weights["skill"] +
+        scores["experience"] * weights["experience"] +
+        scores["education"] * weights["education"]
+        
+    )
+    return round(final, 2)
